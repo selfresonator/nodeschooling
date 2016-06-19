cfb79052835c38e2262123455e7d21f72f59da0b
selfresonator <selfresonator@gmail.com>
2016-06-18 23:10:36 -0700

    Started writing Hash table from scratch

---
+++ hashtable.js
@@ -0,0 +1,20 @@
+(function () {
+  var HashMap = {};
+  var storage  = [];
+  var limit = 4;
+
+  HashMap.insert = function() {
+
+  };
+
+  HashMap.retrieve = function() {
+
+  };
+
+  HashMap.remove = function() {
+
+  };
+
+  return hashMap;
+
+})();
