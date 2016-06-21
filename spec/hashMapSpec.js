1dedf1402a7be1fde49c6dcef222c9a182969474
selfresonator <selfresonator@gmail.com>
2016-06-20 23:37:18 -0700

    Adds test to check what is returned from hashMap

--- spec/hashMapSpec.js
+++ spec/hashMapSpec.js
@@ -1,12 +1,18 @@
+var chai = require('chai');
+var expect = chai.expect;
+
 describe('hashMap', function() {
   var hashMap;
-  // beforeEach(function() {
-  //   hashMap = new HashMap();
-  // });
+  beforeEach(function() {
+    hashMap = new HashMap();
+  });
 
   it('should be a function', function() {
     expect(hashMap).to.be.a("function");
   });
 
+  if('should return a method', function() {
+    expect(hashMap()).to.equal("undefined");
+  });
 
 });
